7828bd39078f8f7cdf3764b7bbcc6d172d30b74e
hamzashahab1610 <hamzashahab1610@gmail.com>
2021-05-04 00:03:57 +0500

    release 5

--- src/App.js
+++ src/App.js
@@ -1,58 +1,11 @@
 import "./App.css";
 import React from "react";
-import Modal from "react-modal";
-import ContactUs from "./components/ContactUs/ContactUs";
-import { useMediaQuery } from "react-responsive";
+import Model from "./components/Model/Model";
 
 function App() {
-	const isTabletOrMobile = useMediaQuery({ query: "(max-width: 768px)" });
-	const [modalIsOpen, setIsOpen] = React.useState(false);
-
-	const customStyles = {
-		content: {
-			top: "50%",
-			left: "50%",
-			right: "auto",
-			bottom: "auto",
-			marginRight: "-50%",
-			transform: "translate(-50%, -50%)",
-			padding: "0px",
-			borderRadius: "0px",
-			width: isTabletOrMobile ? "360px" : "710px",
-			minHeight: isTabletOrMobile ? "100%" : "641px",
-			maxHeight: "100%",
-		},
-	};
-
-	function openModal() {
-		setIsOpen(true);
-	}
-
-	function afterOpenModal() {
-		// references are now sync'd and can be accessed.
-		// subtitle.style.color = "#f00";
-	}
-
-	function closeModal() {
-		setIsOpen(false);
-	}
-
 	return (
 		<div className="App">
-			{!modalIsOpen && (
-				<button className="open-model-btn" onClick={openModal}>
-					Open Modal
-				</button>
-			)}
-			<Modal
-				isOpen={modalIsOpen}
-				onAfterOpen={afterOpenModal}
-				onRequestClose={closeModal}
-				style={customStyles}
-				contentLabel="Example Modal"
-			>
-				<ContactUs closeModal={closeModal} />
-			</Modal>
+			<Model />
 		</div>
 	);
 }
---
+++ src/components/Model/Model.js
@@ -0,0 +1,208 @@
+import React, { useEffect, useState } from "react";
+import "./Model.css";
+import ReCAPTCHA from "react-google-recaptcha";
+import Error from "../../assets/error.svg";
+
+function Model({ closeModal }) {
+	const submitText = "SUBMIT";
+
+	const [name, setName] = useState("");
+	const [email, setEmail] = useState("");
+	const [phone, setPhone] = useState("");
+	const [message, setMessage] = useState("");
+	const [captcha, setCaptcha] = useState();
+	const [emailError, setEmailError] = useState("");
+	const [phoneError, setPhoneError] = useState("");
+	const [msgError, setMsgError] = useState("");
+	const [isSubmitEnabled, setisSubmitEnabled] = useState(false);
+
+	function onChange(value) {
+		console.log("Captcha value:", value);
+		setCaptcha(value);
+	}
+
+	useEffect(() => {
+		if (
+			name !== "" &&
+			email !== "" &&
+			phone !== "" &&
+			message !== "" &&
+			captcha
+		) {
+			setisSubmitEnabled(true);
+		} else {
+			setisSubmitEnabled(false);
+		}
+	});
+
+	const validateEmail = (email) => {
+		var pattern = new RegExp(
+			/^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i,
+		);
+
+		if (!pattern.test(email)) {
+			setEmailError("Review your email format.");
+		} else {
+			setEmailError("");
+		}
+	};
+
+	const validatePhone = (phone) => {
+		var pattern = new RegExp(
+			/^\(?([0-9]{3})\)?[-.\s]([0-9]{3})[-.\s]([0=9]{4})?/,
+		);
+
+		if (!pattern.test(phone)) {
+			setPhoneError("Review your phone format.");
+		} else {
+			setPhoneError("");
+		}
+	};
+
+	const validateMessage = (msg) => {
+		if (msg.length >= 240) {
+			setMsgError(
+				"Your max your character count, please shorten your message.",
+			);
+		} else {
+			setMsgError("");
+		}
+	};
+
+	const submitHandler = (e) => {
+		e.preventDefault();
+		validateEmail(email);
+		validatePhone(phone);
+		validateMessage(message);
+	};
+
+	return (
+		<div className="model-container">
+			<div className="form-container">
+				<div className="close-btn">
+					<i class="fas fa-times"></i>
+				</div>
+				<header className="header">Email Us</header>
+				<div className="sub-wrapper">
+					<div className="input-container">
+						{name !== "" && (
+							<label className="label" htmlFor="">
+								Name
+							</label>
+						)}
+
+						<input
+							className="input"
+							placeholder="Name"
+							value={name}
+							onChange={(e) => setName(e.target.value)}
+						/>
+					</div>
+					<div className="input-container">
+						{phone !== "" && (
+							<label className="label" htmlFor="">
+								Phone Number
+							</label>
+						)}
+						<input
+							type="tel"
+							// pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
+							className="input"
+							placeholder="Enter Phone Number"
+							value={phone}
+							style={{
+								borderColor:
+									phoneError !== "" ? "red" : "#edeeef",
+							}}
+							onChange={(e) => setPhone(e.target.value)}
+						/>
+						{phoneError !== "" && (
+							<div className="error-msg">
+								<img src={Error} alt="" /> {phoneError}
+							</div>
+						)}
+					</div>
+					<div className="input-container">
+						{email !== "" && (
+							<label className="label" htmlFor="">
+								Email
+							</label>
+						)}
+						<input
+							className="input"
+							placeholder="Enter Email"
+							// pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$"
+							value={email}
+							onChange={(e) => setEmail(e.target.value)}
+							style={{
+								borderColor:
+									emailError !== "" ? "red" : "#edeeef",
+							}}
+						/>
+						{emailError !== "" && (
+							<div className="error-msg">
+								<img src={Error} alt="" /> {emailError}
+							</div>
+						)}
+					</div>
+					<div className="input-container">
+						{message !== "" && (
+							<label className="label" htmlFor="">
+								Message
+							</label>
+						)}
+						<textarea
+							className="text-area"
+							placeholder="Enter your Message"
+							maxlength="240"
+							value={message}
+							onChange={(e) => {
+								validateMessage(e.target.value);
+								setMessage(e.target.value);
+							}}
+							style={{
+								borderColor:
+									msgError !== "" ? "red" : "#edeeef",
+							}}
+						/>
+						<div id="the-count">
+							<span id="current">{message.length}</span>
+							<span id="maximum">/ 240</span>
+						</div>
+					</div>
+					{msgError !== "" && (
+						<div className="error-msg">
+							<img src={Error} alt="" /> {msgError}
+						</div>
+					)}
+
+					<div className="recaptcha">
+						<ReCAPTCHA
+							sitekey="6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI"
+							onChange={onChange}
+						/>
+					</div>
+				</div>
+				<div className="buttons">
+					<button type="reset" className="cancel-btn">
+						CANCEL
+					</button>
+					<button
+						// type="submit"
+						className={
+							isSubmitEnabled
+								? "submit-btn"
+								: "submit-btn-disabled"
+						}
+						onClick={submitHandler}
+						disabled={!isSubmitEnabled}
+					>
+						{submitText}
+					</button>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default Model;
